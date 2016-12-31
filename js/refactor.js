4b65917cef821b660b83f1dacc764c8e3470fdf3
sv <sv@linus.attlocal.net>
2016-12-30 22:28:35 -0800

    while

--- js/refactor.js
+++ js/refactor.js
@@ -3,7 +3,7 @@ var red;
 var green;
 var blue;
 var rgbColor;
-
+/*
 red = Math.floor(Math.random() * 256 );
 green = Math.floor(Math.random() * 256 );
 blue = Math.floor(Math.random() * 256 );
@@ -63,5 +63,16 @@ green = Math.floor(Math.random() * 256 );
 blue = Math.floor(Math.random() * 256 );
 rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 html += '<div style="background-color:' + rgbColor + '"></div>';
+*/
+function randCC (){
+	red = Math.floor(Math.random() * 256 );
+	green = Math.floor(Math.random() * 256 );
+	blue = Math.floor(Math.random() * 256 );
+	rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+	html += '<div style="background-color:' + rgbColor + '"></div>';
+}
+var i = 0;
+while (i++ < 10)
+	randCC();
 
 document.write(html);
